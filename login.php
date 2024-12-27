0e9cb6b5c7b60b0e3196a642cf5a33ee8116e7b4
Karan Rawat <karan.rawat@san.soft>
2024-12-27 18:31:04 +0530

    From office 2

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery.js"></script>
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="login-wrapper">
+
+
+        <div class="login-box-container container">
+
+            <div class="col image_box"></div>
+            <div class="col form_box">
+                <form></form>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/login.js"></script>
+</body>
+</html>
