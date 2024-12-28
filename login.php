8e5f3f99fba3b2eabc9bb099e9b2d722de793c5c
karanroliyal <karanrawat.bca2021@imsuc.ac.in>
2024-12-28 08:36:38 +0530

    from pg

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery.js"></script>
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="login-wrapper">
+
+
+        <div class="login-box-container container">
+
+            <div class="col image_box"></div>
+            <div class="col form_box">
+                <div class="my-logo-container">
+                    <img src="assets/images/logo.png" alt="logo">
+                </div>
+                <h4>Log in</h4>
+                <form id="login_form">
+                    <input type="text" placeholder="Uername or email">
+                    <input type="password" placeholder="Enter your password">
+                    <button type="button">Login</button>
+                </form>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/login.js"></script>
+</body>
+</html>
